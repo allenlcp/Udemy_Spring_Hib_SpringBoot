bb4d27072463560c6be02605a5a11a67be691365
allenlcp <allen.decodemtl@gmail.com>
2019-02-26 00:10:56 -0500

    finishing user registration - customer

--- section055_user_registration/src/main/java/com/company/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.company.springsecurity.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    // No coding required
-}
